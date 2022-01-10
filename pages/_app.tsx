b196970ebcf1f5d55f645a9b64baa52866fb9eae
ChamikaAnd <chamika.adikari@and.digital>
2022-01-10 22:01:44 +0000

    Create a layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,19 @@
+import type { ReactElement, ReactNode } from 'react';
+import type { NextPage } from 'next'
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+type NextPageWithLayout = NextPage & {
+  getLayout?: (page: ReactElement) => ReactNode
 }
 
-export default MyApp
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout
+}
+
+function MyApp({ Component, pageProps }: AppPropsWithLayout) {
+  const getLayout = Component.getLayout ?? ((page) => page)
+  return getLayout(<Component {...pageProps} />)
+}
+
+export default MyApp;
