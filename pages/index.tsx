9dff4f35c36ee316948c6e26b99a328277b12b3e
ChamikaAnd <chamika.adikari@and.digital>
2022-01-13 23:40:06 +0000

    fix build issue

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,7 +9,7 @@ export default function Page(){
       <div className='flex flex-col-reverse sm:flex-row'>
         <div>
           <h1 className="text-5xl font-bold mb-1">
-            Hi, I'm Chamika
+            Hi, Im Chamika
           </h1>
           <p className='text-gray-700 mb-4'>Product Developer at And Digital</p>
           <br />
@@ -28,19 +28,19 @@ export default function Page(){
         <div>
           <div className='mt-12'>
             <h4 className='text-lg font-bold'>My Custom CSS Reset</h4>
-            <p className='mt-4'>I have a set of baseline CSS styles that come with me from project to project. In the past, I'd use a typical CSS reset, but times have changed, and I believe I have a better set of global styles!</p>
+            <p className='mt-4'>I have a set of baseline CSS styles that come with me from project to project. In the past, Id use a typical CSS reset, but times have changed, and I believe I have a better set of global styles!</p>
           </div>
           <div className='mt-12'>
             <h4 className='text-lg font-bold'>My Custom CSS Reset</h4>
-            <p className='mt-4'>I have a set of baseline CSS styles that come with me from project to project. In the past, I'd use a typical CSS reset, but times have changed, and I believe I have a better set of global styles!</p>
+            <p className='mt-4'>I have a set of baseline CSS styles that come with me from project to project. In the past, Id use a typical CSS reset, but times have changed, and I believe I have a better set of global styles!</p>
           </div>
           <div className='mt-12'>
             <h4 className='text-lg font-bold'>My Custom CSS Reset</h4>
-            <p className='mt-4'>I have a set of baseline CSS styles that come with me from project to project. In the past, I'd use a typical CSS reset, but times have changed, and I believe I have a better set of global styles!</p>
+            <p className='mt-4'>I have a set of baseline CSS styles that come with me from project to project. In the past, Id use a typical CSS reset, but times have changed, and I believe I have a better set of global styles!</p>
           </div>
           <div className='mt-12'>
             <h4 className='text-lg font-bold'>My Custom CSS Reset</h4>
-            <p className='mt-4'>I have a set of baseline CSS styles that come with me from project to project. In the past, I'd use a typical CSS reset, but times have changed, and I believe I have a better set of global styles!</p>
+            <p className='mt-4'>I have a set of baseline CSS styles that come with me from project to project. In the past, Id use a typical CSS reset, but times have changed, and I believe I have a better set of global styles!</p>
           </div>
         </div>
       </div>
