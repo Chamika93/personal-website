349b413327ab419fe40144b4b2a3ae12b9a655a9
ChamikaAnd <chamika.adikari@and.digital>
2022-01-14 01:18:07 +0000

    comment links

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Link from 'next/Link';
+// import Link from 'next/Link';
 
 
 function Header() {
@@ -8,10 +8,10 @@ function Header() {
             <nav className='flex items-center justify-between w-full pt-8 pb-8 sm:pb-16 max-w-2xl mx-auto'>
                 <div>
                     {/* Need to add hidden hamburger icon */}
-                    <Link href="/"><a className="pr-3 py-2 font-bold">Home</a></Link>
+                    {/* <Link href="/"><a className="pr-3 py-2 font-bold">Home</a></Link>
                     <Link href="/about"><a className="px-3 py-2">Projects</a></Link>
                     <Link href="/users"><a className="px-3 py-2">Blog</a></Link>
-                    <Link href="/users"><a className="px-3 py-2">Snippets</a></Link>
+                    <Link href="/users"><a className="px-3 py-2">Snippets</a></Link> */}
                 </div>
                 <button>Test</button>
             </nav>
